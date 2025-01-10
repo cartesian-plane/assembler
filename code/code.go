7429b6bce8e29b815b7046f780deb5da00b4c263
Andrei <87477311+cartesian-plane@users.noreply.github.com>
2025-01-10 11:27:22 +0200

    Add code.go

---
+++ code/code.go
@@ -0,0 +1,106 @@
+package code
+
+import (
+	"fmt"
+	"strings"
+)
+
+func Dest(symbol string) string {
+	code := []byte("000")
+	if strings.ContainsRune(symbol, 'M') {
+		code[2] = '1'
+	}
+	if strings.ContainsRune(symbol, 'D') {
+		code[1] = '1'
+	}
+	if strings.ContainsRune(symbol, 'A') {
+		code[0] = '1'
+	}
+	return string(code)
+}
+
+func Comp(symbol string) string {
+	switch symbol {
+	case "0":
+		return "0101010"
+	case "1":
+		return "1111111"
+	case "-1":
+		return "1111010"
+	case "D":
+		return "1001100"
+	case "A":
+		return "0110000"
+	case "M":
+		return "1110000"
+	case "!D":
+		return "001111"
+	case "!A":
+		return "0110001"
+	case "!M":
+		return "1110001"
+	case "-D":
+		return "0001111"
+	case "-A":
+		return "0110011"
+	case "-M":
+		return "1110011"
+	case "D+1":
+		return "0011111"
+	case "A+1":
+		return "0110111"
+	case "M+1":
+		return "1110111"
+	case "D-1":
+		return "0001110"
+	case "A-1":
+		return "0110010"
+	case "M-1":
+		return "1110010"
+	case "D+A":
+		return "0000010"
+	case "D+M":
+		return "1000010"
+	case "D-A":
+		return "0010011"
+	case "D-M":
+		return "1010011"
+	case "A-D":
+		return "0000111"
+	case "M-D":
+		return "1000111"
+	case "D&A":
+		return "0000000"
+	case "D&M":
+		return "1000000"
+	case "D|A":
+		return "0010101"
+	case "D|M":
+		return "1010101"
+	default:
+		panic(fmt.Errorf("unknown comp symbol: %s", symbol))
+	}
+}
+
+func Jump(symbol string) string {
+	switch symbol {
+	case "":
+		return "000"
+	case "JGT":
+		return "001"
+	case "JEQ":
+		return "010"
+	case "JGE":
+		return "011"
+	case "JLT":
+		return "100"
+	case "JNE":
+		return "101"
+	case "JLE":
+		return "110"
+	case "JMP":
+		return "111"
+	default:
+		panic(fmt.Errorf("unknown jump symbol: %s", symbol))
+	}
+}
